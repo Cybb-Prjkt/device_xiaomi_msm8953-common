9efe65b2e8812a3d3d15b96822c6609a975cca04
Giovanni Ricca <giovanniricca@proton.me>
2024-11-29 16:21:18 +0100

    msm8953-common: Decommonize screen density
    Change-Id: I0c2c2723c0e9826c549e86dd591bc64893ce98cf
    (cherry picked from commit 6e43c1771414759531cb84703ac6c3804369edc3)

--- msm8953.mk
+++ msm8953.mk
@@ -17,14 +17,6 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/Car
 # APEX
 OVERRIDE_PRODUCT_COMPRESSED_APEX := false
 
-# Screen density
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG ?= xxhdpi
-
-# Boot animation
-TARGET_SCREEN_HEIGHT ?= 1920
-TARGET_SCREEN_WIDTH ?= 1080
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
